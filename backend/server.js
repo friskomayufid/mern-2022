1e9a06e50eceb122e74039d576f754286c090804
Frisko <frisko@koinworks.com>
2022-02-08 21:45:15 +0700

    route and controller setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,17 @@
-console.log('Hellow')
+const express = require('express')
+const router = require('./routes/goalRoutes')
+const { errorHandler } = require('./middleware/errorMiddleware')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/api/goals', router)
+app.use(errorHandler)
+
+app.listen(port, () => {
+  console.log(`Server started on port http://localhost:${port}`)
+})
